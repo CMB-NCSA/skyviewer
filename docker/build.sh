c3c3192dbdda50927b557ad68fd90fcaf7f79914
Felipe Menanteau <felipe@illinois.edu>
2023-05-26 10:51:10 -0500

    Re-org

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,4 @@ export IMAGE=skyviewer_aladin
 export SPTUSER=$USER
 
 export TAG=test
-docker build -f ./Dockerfile_ubuntu -t menanteau/$IMAGE:$TAG --build-arg SPTUSER --rm=true .
+docker build -f ./ubuntu/Dockerfile -t menanteau/$IMAGE:$TAG --build-arg SPTUSER --rm=true .
