8d9352706fff0afbf76f8b19e56629c859fdc083
Felipe Menanteau <felipe@illinois.edu>
2023-06-15 12:58:20 -0500

    Updates to splitview

--- www/resources_splitviews/app.js
+++ www/resources_splitviews/app.js
@@ -1,9 +1,40 @@
-var a1 = A.aladin('#al1', {cooFrame: 'equatorial', projection: "ZEA", fov: 90, realFullscreen: true, target: '0 0 0 -56 45 0', survey: 'P/DES-DR2/ColorIRG'});
+var a1 = A.aladin('#al1',
+  {
+    cooFrame: 'equatorial',
+    projection: "ZEA",
+    fov: 90,
+    realFullscreen: true,
+    target: '0 0 0 -56 45 0',
+    survey: 'P/DES-DR2/ColorIRG'
+  }
+);
+a1.createImageSurvey("P/DES-DR2/ColorIRG", "DES-DR2 ColorIRG", "https://alasky.cds.unistra.fr/DES/DR2/CDS_P_DES-DR2_ColorIRG", "equatorial", 11, {imgFormat: "png"});
+a1.setBaseImageLayer("P/DES-DR2/ColorIRG",{imgFormat: "png"});
 a1.setFovRange(0.01, 175);
-//a1.gotoRaDec(297.87, 25.96);
-var a2 = A.aladin('#al2', {cooFrame: 'equatorial', projection: "ZEA", fov: 90, realFullscreen: true, target: '0 0 0 -56 45 0', survey: 'P/SPT/WINTER2020-150GHZ', showFrameControl: false, showFullscreenControl: false, showGotoControl: false});
-a2.setFovRange(0.01, 175);
 
+var hipsDir090 = `./hips/SPT_winter2020_090GHz_HiPS/`;
+hipsDir090 = hipsDir090.substring(0,hipsDir090.lastIndexOf("/",hipsDir090.length));
+var hipsDir150 = `./hips/SPT_winter2020_150GHz_HiPS/`;
+hipsDir150 = hipsDir150.substring(0,hipsDir150.lastIndexOf("/",hipsDir150.length));
+var hipsDir220 = `./hips/SPT_winter2020_220GHz_HiPS/`;
+hipsDir220 = hipsDir220.substring(0,hipsDir220.lastIndexOf("/",hipsDir220.length));
+
+var a2 = A.aladin('#al2',
+  {
+   cooFrame: 'equatorial',
+   projection: "ZEA",
+   fov: 90,
+   realFullscreen: true,
+   target: '0 0 0 -56 45 0',
+   showFrameControl: false, showFullscreenControl: false, showGotoControl: false,
+ }
+);
+
+a2.createImageSurvey('SPT_winter2020_090GHz0_HiPS', 'SPT Winter2020 090GHz', hipsDir090, 'equatorial', 5, {imgFormat: 'png'});
+a2.createImageSurvey('SPT_winter2020_150GHz0_HiPS', 'SPT Winter2020 150GHz', hipsDir150, 'equatorial', 5, {imgFormat: 'png'});
+a2.createImageSurvey('SPT_winter2020_220GHz0_HiPS', 'SPT Winter2020 200GHz', hipsDir220, 'equatorial', 5, {imgFormat: 'png'});
+a2.setBaseImageLayer("SPT_winter2020_150GHz0_HiPS",{imgFormat: "png"});
+a2.getBaseImageLayer().getColorMap().update('rainbow')
 
 View.CALLBACKS_THROTTLE_TIME = 30;
 a1.on('positionChanged', function(params) {
